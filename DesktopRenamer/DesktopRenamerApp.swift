c465fc7656d9dd872ad98d8ac8340703aebd1fe2
michaelqiu <altmickymike@outlook.com>
2025-07-20 11:13:48 +0900

    Init commit

---
+++ DesktopRenamer/DesktopRenamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesktopRenamerApp.swift
+//  DesktopRenamer
+//
+//  Created by Michael Qiu on 2025/7/20.
+//
+
+import SwiftUI
+
+@main
+struct DesktopRenamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DesktopRenamerTests/DesktopRenamerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DesktopRenamerTests.swift
+//  DesktopRenamerTests
+//
+//  Created by Michael Qiu on 2025/7/20.
+//
+
+import Testing
+@testable import DesktopRenamer
+
+struct DesktopRenamerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
