7064d346761909ee07992ffd4b7f0167d3a997fa
michaelqiu <altmickymike@outlook.com>
2025-07-24 00:22:39 -0400

    Adjust DLW text layout

--- DesktopRenamer/Views/DesktopLabelWindow.swift
+++ DesktopRenamer/Views/DesktopLabelWindow.swift
@@ -13,13 +13,40 @@ class DesktopLabelWindow: NSWindow {
         
         // Create the label
         label = NSTextField(labelWithString: name)
-        label.font = .systemFont(ofSize: 50, weight: .medium)
+        label.font = .systemFont(ofSize: 50, weight: .medium) // Initial font size, will be adjusted
         label.textColor = .white
         label.alignment = .center
-        label.frame = NSRect(x: 0, y: 0, width: 400, height: 300)
         
         // Create a visual effect view for the background
         let visualEffect = NSVisualEffectView(frame: NSRect(x: 0, y: 0, width: 400, height: 300))
+        
+        // Calculate and set optimal font size and frame
+        let padding: CGFloat = 20
+        let maxWidth = visualEffect.frame.width - (padding * 2)
+        let maxHeight = visualEffect.frame.height - (padding * 2)
+        
+        // Start with initial font size and adjust down if needed
+        var fontSize: CGFloat = 50
+        var attributedString = NSAttributedString(string: name, attributes: [.font: NSFont.systemFont(ofSize: fontSize, weight: .medium)])
+        var stringSize = attributedString.size()
+        
+        // Reduce font size until text fits within bounds with some padding
+        while (stringSize.width > maxWidth || stringSize.height > maxHeight) && fontSize > 10 {
+            fontSize -= 2
+            attributedString = NSAttributedString(string: name, attributes: [.font: NSFont.systemFont(ofSize: fontSize, weight: .medium)])
+            stringSize = attributedString.size()
+        }
+        
+        label.font = .systemFont(ofSize: fontSize, weight: .medium)
+        
+        // Center the label in the visual effect view
+        let labelFrame = NSRect(
+            x: (visualEffect.frame.width - stringSize.width) / 2,
+            y: (visualEffect.frame.height - stringSize.height) / 2,
+            width: stringSize.width,
+            height: stringSize.height
+        )
+        label.frame = labelFrame
         visualEffect.material = .hudWindow
         visualEffect.state = .active
         visualEffect.wantsLayer = true
@@ -89,7 +116,34 @@ class DesktopLabelWindow: NSWindow {
     
     func updateName(_ name: String) {
         DispatchQueue.main.async {
+            // Calculate optimal font size for new name
+            let padding: CGFloat = 20
+            let maxWidth = self.contentView?.frame.width ?? 400 - (padding * 2)
+            let maxHeight = self.contentView?.frame.height ?? 300 - (padding * 2)
+            
+            var fontSize: CGFloat = 50
+            var attributedString = NSAttributedString(string: name, attributes: [.font: NSFont.systemFont(ofSize: fontSize, weight: .medium)])
+            var stringSize = attributedString.size()
+            
+            while (stringSize.width > maxWidth || stringSize.height > maxHeight) && fontSize > 10 {
+                fontSize -= 2
+                attributedString = NSAttributedString(string: name, attributes: [.font: NSFont.systemFont(ofSize: fontSize, weight: .medium)])
+                stringSize = attributedString.size()
+            }
+            
+            self.label.font = .systemFont(ofSize: fontSize, weight: .medium)
             self.label.stringValue = name
+            
+            // Recenter the label
+            if let visualEffect = self.contentView as? NSVisualEffectView {
+                let labelFrame = NSRect(
+                    x: (visualEffect.frame.width - stringSize.width) / 2,
+                    y: (visualEffect.frame.height - stringSize.height) / 2,
+                    width: stringSize.width,
+                    height: stringSize.height
+                )
+                self.label.frame = labelFrame
+            }
         }
     }
     
