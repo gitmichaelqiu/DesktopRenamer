7904bb9ca0303799820aaad95462b3a615f6358b
michaelqiu <altmickymike@outlook.com>
2025-08-23 00:03:55 +0800

    Add auto-update

--- DesktopRenamer/DesktopRenamerApp.swift
+++ DesktopRenamer/DesktopRenamerApp.swift
@@ -16,6 +16,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Initialize SpaceManager and StatusBarController
         spaceManager = SpaceManager()
         statusBarController = StatusBarController(spaceManager: spaceManager)
+
+        // Automatically check for updates on launch if enabled
+        if UpdateManager.isAutoCheckEnabled {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                UpdateManager.shared.checkForUpdate(from: nil, suppressUpToDateAlert: true)
+            }
+        }
     }
 }
 
