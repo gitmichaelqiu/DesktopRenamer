5bddb1cdea2bbc74e9dded79d1f87fd9c984a18a
Michael Y. Qiu <altmickymike@outlook.com>
2025-12-20 19:42:05 +0800

    Adjust default values

--- DesktopRenamer/Services/UpdateManager.swift
+++ DesktopRenamer/Services/UpdateManager.swift
@@ -29,7 +29,12 @@ class UpdateManager {
     // UserDefaults key for auto update check
     static let autoCheckKey = "AutoCheckForUpdate"
     static var isAutoCheckEnabled: Bool {
-        get { UserDefaults.standard.bool(forKey: autoCheckKey) }
+        get {
+            if UserDefaults.standard.object(forKey: autoCheckKey) == nil {
+                return true
+            }
+            return UserDefaults.standard.bool(forKey: autoCheckKey)
+        }
         set { UserDefaults.standard.set(newValue, forKey: autoCheckKey) }
     }
     
