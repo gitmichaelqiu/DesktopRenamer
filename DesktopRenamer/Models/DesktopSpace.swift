6def8d61489787321b1a5632d6111c226cc31672
Michael Y. Qiu <altmickymike@outlook.com>
2026-01-20 18:32:09 +0800

    Allow switching to fullscreen

--- DesktopRenamer/Models/DesktopSpace.swift
+++ DesktopRenamer/Models/DesktopSpace.swift
@@ -5,22 +5,24 @@ struct DesktopSpace: Identifiable, Codable, Equatable {
     var customName: String
     var num: Int
     var displayID: String
+    var isFullscreen: Bool
     
-    // Custom decoding to handle legacy data without displayID
+    // Custom decoding to handle legacy data
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         id = try container.decode(String.self, forKey: .id)
         customName = try container.decode(String.self, forKey: .customName)
         num = try container.decode(Int.self, forKey: .num)
-        // Default to "Main" if displayID is missing in old data
         displayID = try container.decodeIfPresent(String.self, forKey: .displayID) ?? "Main"
+        isFullscreen = try container.decodeIfPresent(Bool.self, forKey: .isFullscreen) ?? false
     }
     
     // Default init
-    init(id: String, customName: String, num: Int, displayID: String) {
+    init(id: String, customName: String, num: Int, displayID: String, isFullscreen: Bool = false) {
         self.id = id
         self.customName = customName
         self.num = num
         self.displayID = displayID
+        self.isFullscreen = isFullscreen
     }
 }
