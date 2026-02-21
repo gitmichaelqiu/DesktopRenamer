daea5cbb0d7be782083fcc00160fb1510ede1c79
Michael Y. Qiu <altmickymike@outlook.com>
2026-02-21 19:56:30 +0800

    Update signature script

--- test_entitlements.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Test extracting and modifying entitlements
-APP_PATH="$(pwd)/build/Release/DesktopRenamer.app"
-codesign -d --entitlements :- "$APP_PATH" > temp.entitlements
-plutil -remove com.apple.security.application-groups temp.entitlements
-cat temp.entitlements
-rm temp.entitlements
