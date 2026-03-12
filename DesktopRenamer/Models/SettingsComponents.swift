7fdb069ca2eb0f83181fd6a994f98298c294a7c1
Michael Y. Qiu <altmickymike@outlook.com>
2026-03-12 23:21:02 +0800

    Reduce nil sec padding

--- DesktopRenamer/Models/SettingsComponents.swift
+++ DesktopRenamer/Models/SettingsComponents.swift
@@ -81,6 +81,8 @@ struct SettingsSection<Content: View>: View {
                     )
             )
         }
+        .padding(.top, title == nil ? -10 : 0)
+        .padding(.bottom, title == nil ? 10 : 0)
     }
 
     private var backgroundColor: Color {
