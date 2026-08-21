d9cb52333bd996a82046e09ccf24b9b820f78d40
Michael Y. Qiu <altmickymike@outlook.com>
2026-08-21 16:56:29 +0800

    fix: support macOS 13 settings callbacks

--- DesktopRenamer/Models/SettingsComponents.swift
+++ DesktopRenamer/Models/SettingsComponents.swift
@@ -15,7 +15,7 @@ private struct AnimatedSettingsValue: View {
         Text(displayedText)
             .monospacedDigit()
             .contentTransition(.numericText())
-            .onChange(of: text) { _, newText in
+            .onChange(of: text) { newText in
                 withSettingsAnimation {
                     displayedText = newText
                 }
@@ -284,7 +284,7 @@ struct SettingsContainer<Content: View>: View {
                     .padding(16)
             }
             .environment(\.settingsTab, tab)
-            .onChange(of: navigationState.scrollToItemID) { _, id in
+            .onChange(of: navigationState.scrollToItemID) { id in
                 if let id = id {
                     DispatchQueue.main.asyncAfter(deadline: .now() + 0.15) {
                         withAnimation {
