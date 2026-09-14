f8a4ebc44b1fd314956492f3126681f933a262fb
Michael Y. Qiu <altmickymike@outlook.com>
2026-09-15 00:20:58 +0800

    fix: refine launcher submenu spacing

--- DesktopRenamer/Views/Launcher/LauncherComponents.swift
+++ DesktopRenamer/Views/Launcher/LauncherComponents.swift
@@ -15,8 +15,8 @@ enum LauncherLayout {
     static let submenuHeaderIconSlot: CGFloat = rowIconSlot
     static let submenuHeaderSpacing: CGFloat = 10
     static let submenuRowContentSpacing: CGFloat = 6
-    static let submenuHeaderHorizontalPadding: CGFloat = 16
-    static let submenuHeaderVerticalPadding: CGFloat = 14
+    static let submenuHeaderHorizontalPadding: CGFloat = submenuRowHorizontalPadding
+    static let submenuHeaderVerticalPadding: CGFloat = 12
     static let submenuSeparatorSpacing: CGFloat = 6
     static let submenuRowHorizontalPadding: CGFloat = 8
     static let submenuRowHeight: CGFloat = 36
