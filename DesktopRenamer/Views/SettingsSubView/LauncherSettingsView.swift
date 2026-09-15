e0aa8911fe087f20689a57b34ee215adf111f92c
Michael Y. Qiu <altmickymike@outlook.com>
2026-09-16 07:53:13 +0800

    fix: match launcher settings row heights

--- DesktopRenamer/Views/SettingsSubView/LauncherSettingsView.swift
+++ DesktopRenamer/Views/SettingsSubView/LauncherSettingsView.swift
@@ -150,6 +150,7 @@ struct LauncherSettingsView: View {
             }
             .padding(.horizontal, 12)
             .padding(.vertical, 6)
+            .frame(minHeight: 44)
 
             if !isLast {
                 Divider().padding(.leading, 12)
@@ -177,7 +178,7 @@ struct LauncherSettingsView: View {
         }
         .padding(.horizontal, 12)
         .padding(.vertical, 6)
-        .frame(minWidth: 320, alignment: .leading)
+        .frame(minWidth: 320, minHeight: 44, alignment: .leading)
         .contentShape(.dragPreview, Rectangle())
     }
 
